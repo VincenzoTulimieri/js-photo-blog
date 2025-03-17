e259d41e2fe1abf831e91b1405934e2e5303a834
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-03-17 11:02:07 +0100

    feat: aggiunto data e descrizione alle card

--- js/app.js
+++ js/app.js
@@ -14,13 +14,16 @@ for (let i = 0; i < 6; i++) {
         .then(response => {
             const elementImg = response.data[i].url
             const elementTitle = response.data[i].title
+            const elementDate = response.data[i].date
+            console.log(elementDate)
             console.log(elementTitle)
             items+=`<div class="col-sm-12 col-md-6 col-lg-4">
                     <div class="card p-3" style="width: 18rem;">
                         <img src="${elementImg}" alt="">
                         <img class="translate-middle start-50" src="img/pin.svg" style="position:absolute;" alt="pin">
                         <div class="card-body">
-                          <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, facere.</p>
+                          <p class="card-text vt-text-date vt-text"><strong>${elementDate}</strong></p>
+                          <p class="card-text vt-text-description vt-title"><strong>${elementTitle}</strong></p>
                         </div>
                     </div>
                 </div>`
@@ -29,7 +32,7 @@ for (let i = 0; i < 6; i++) {
             console.log(error)
         })
         
-    }
+}
     
 
   
